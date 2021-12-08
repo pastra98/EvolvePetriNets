4da3a6f27b249d596a0b2c6a6e45ff46fb53c769
Paul Straberger <p.straberger@gmail.com>
2021-12-08 12:33:13 +0100

    fingers crossed

--- src/tests/read_and_save_new_params.py
+++
@@ -1,8 +0,0 @@
-from src.neat import params
-import importlib
-importlib.reload(params)
-
-def save_new_params(old_name, new_name, save_current=True):
-    params.load(old_name)
-    print(params.popsize)
-    params.new_param_json(new_name, save_current=True)
