d14e29c25e4b6308b7f4c1fd565f1d26a1e8b9b3
Paul Straberger <p.straberger@gmail.com>
2021-12-10 11:58:40 +0100

    add stuff to analysis

--- results/analysis_scripts/analysis.py
+++ results/analysis_scripts/analysis.py
@@ -10,6 +10,10 @@ if not os.getcwd().endswith("genetic_miner"):
     sys.path.append(str(cwd.parent.parent / "src")) # src from where all the relative imports work
     os.chdir(cwd.parent.parent) # workspace level from where I execute scripts
 
+# load pickle from fname, assign name d
+import pickle as pkl
+import pandas as pd
+
 # %%
 # list available files
 files = [f"results/data/{f}" for f in os.listdir("results/data")]
@@ -17,19 +21,66 @@ print(f"files in data:")
 print('\n'.join(files))
 
 # %%
-# load pickle from fname, assign name d
-import pickle as pkl
 
+def plot_basic_info():
+    pass
+
+def get_run_df(picklename):
+    with open(picklename, "rb") as f:
+        d = pkl.load(f)
+    dlist = []
+    excludes = ["population", "species", "best genome", "times"]
+    for gen, info_dict in d["speciation_params_ga_params"].items():
+        if gen != "time took":
+            if int(gen) > 0:
+                d = {k: info_dict[k] for k in info_dict if k not in excludes}
+                d["gen"] = int(gen)
+                times = info_dict["times"]
+                d = d | {k: times[k] for k in times}
+                dlist.append(d)
+    df = pd.DataFrame(dlist)
+    return df.set_index("gen")
+
+# %%
+
+df_small = get_run_df("results/data/12-09-2021_20-53-10_results.pkl")
+df_large = get_run_df("results/data/12-10-2021_00-19-08_results.pkl")
+
+# %%
+plotvars = {
+    "fitplotvars" : ["best species avg fitness", "best genome fitness", "avg pop fitness"],
+    "timeplotvars" : ["pop_update", "evaluate_curr_generation"],
+    "speciesplotvars" : ["num total species"],
+    "innovplotvars" : ["num new innovations"],
+}
+
+for name, vars in plotvars.items():
+    print("small")
+    df_small[vars].plot(title=name)
+    print("large")
+    df_large[vars].plot(title=name)
+
+# %%
 # fname = files[-1]
-fname = "results/data/12-08-2021_17-04-34_results.pkl"
+# fname = "results/data/12-08-2021_17-04-34_results.pkl"
+
+fname = "results/data/12-09-2021_22-55-41_results.pkl"
 
 with open(fname, "rb") as f:
     print(f"open file:\n{fname}")
     d = pkl.load(f)
 
+for k in d:
+    print(k)
+    # print(d[k])
+    d2 = d[k]
+    for k2 in d2:
+        print(k2)
+        # print(d2[k2])
 
 # %%
 ga_inst = d['speciation_params_ga_params']
+print("small")
 last_gen = ga_inst[0]
 
 # %%
