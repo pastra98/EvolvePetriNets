52b255e3dd5c680f4a8fe4d774947a019919554b
Paul Straberger <p.straberger@gmail.com>
2021-12-11 12:32:28 +0100

    reorg analysis file

--- results/analysis_scripts/analysis.py
+++ results/analysis_scripts/analysis.py
@@ -10,9 +10,10 @@ if not os.getcwd().endswith("genetic_miner"):
     sys.path.append(str(cwd.parent.parent / "src")) # src from where all the relative imports work
     os.chdir(cwd.parent.parent) # workspace level from where I execute scripts
 
-# load pickle from fname, assign name d
+# import various other shit
 import pickle as pkl
 import pandas as pd
+from src.tests import visualize_genome as vg
 
 # %%
 # list available files
@@ -21,78 +22,39 @@ print(f"files in data:")
 print('\n'.join(files))
 
 # %%
+# define used funcs
 
-def plot_basic_info():
-    pass
+def plot_run_df(df):
+    plotvars = {
+        "fitplotvars" : ["best species avg fitness", "best genome fitness", "avg pop fitness"],
+        "timeplotvars" : ["pop_update", "evaluate_curr_generation"],
+        "speciesplotvars" : ["num total species"],
+        "innovplotvars" : ["num new innovations"],
+    }
+    for name, vars in plotvars.items():
+        df[vars].plot(title=name)
+    
 
 def get_run_df(picklename):
+    d = get_unpickled(picklename)
+    df = pickle_to_df(d)
+    return df
+
+
+def get_unpickled(picklename):
     with open(picklename, "rb") as f:
         d = pkl.load(f)
-    dlist = []
-    excludes = ["population", "species", "best genome", "times"]
-    for gen, info_dict in d["speciation_params_ga_params"].items():
-        if gen != "time took":
-            if int(gen) > 0:
-                d = {k: info_dict[k] for k in info_dict if k not in excludes}
-                d["gen"] = int(gen)
-                times = info_dict["times"]
-                d = d | {k: times[k] for k in times}
-                dlist.append(d)
-    df = pd.DataFrame(dlist)
-    return df.set_index("gen")
+    return d
 
-# %%
 
-df_small = get_run_df("results/data/12-09-2021_20-53-10_results.pkl")
-df_large = get_run_df("results/data/12-10-2021_00-19-08_results.pkl")
+def get_gen_species_from_pickle(picklef, gen):
+    gen = picklef["speciation_params_ga_params"][gen]
+    return gen["species"]
 
-# %%
-plotvars = {
-    "fitplotvars" : ["best species avg fitness", "best genome fitness", "avg pop fitness"],
-    "timeplotvars" : ["pop_update", "evaluate_curr_generation"],
-    "speciesplotvars" : ["num total species"],
-    "innovplotvars" : ["num new innovations"],
-}
 
-for name, vars in plotvars.items():
-    print("small")
-    df_small[vars].plot(title=name)
-    print("large")
-    df_large[vars].plot(title=name)
-
-# %%
-# fname = files[-1]
-# fname = "results/data/12-08-2021_17-04-34_results.pkl"
-
-fname = "results/data/12-09-2021_22-55-41_results.pkl"
-
-with open(fname, "rb") as f:
-    print(f"open file:\n{fname}")
-    d = pkl.load(f)
-
-for k in d:
-    print(k)
-    # print(d[k])
-    d2 = d[k]
-    for k2 in d2:
-        print(k2)
-        # print(d2[k2])
-
-# %%
-ga_inst = d['speciation_params_ga_params']
-print("small")
-last_gen = ga_inst[0]
-
-# %%
-for genome in last_gen["population"]:
-    # vg.show_graphviz(genome)
-    vg.show_graphviz(genome)
-
-# %%
-from src.tests import visualize_genome as vg
-
-def info_about_gen(gen: int):
+def print_info_about_gen(picklef, gen: int):
     print(f"showing info about generation {gen}:")
+    ga_inst = picklef['speciation_params_ga_params']
     for s in ga_inst[gen]["species"]:
         print(f"{80*'-'}\n{80*'-'}")
         print(f"{s.name} - Age: {s.age} - num members: {len(s.alive_members)}")
@@ -119,8 +81,35 @@ def info_about_gen(gen: int):
             print()
         print(f"{80*'-'}\n{80*'-'}")
 
+
+def pickle_to_df(picklef):
+    dlist = []
+    excludes = ["population", "species", "best genome", "times"]
+    for gen, info_dict in picklef["speciation_params_ga_params"].items():
+        if gen > 0: # fix this one day
+            d = {k: info_dict[k] for k in info_dict if k not in excludes}
+            d["gen"] = int(gen)
+            times = info_dict["times"]
+            d = d | {k: times[k] for k in times}
+            dlist.append(d)
+    df = pd.DataFrame(dlist)
+    return df.set_index("gen")
+
+
+def plot_species(picklef):
+    return # TODO implement
+
 # %%
-info_about_gen(20)
+# df = get_run_df()
+pf = get_unpickled("results/data/12-10-2021_21-57-14_results.pkl")
+df = pickle_to_df(pf)
+# %%
+print_info_about_gen(pf, 10)
+
+# %%
+for genome in last_gen["population"]:
+    # vg.show_graphviz(genome)
+    vg.show_graphviz(genome)
 
 # %%
 
@@ -143,3 +132,15 @@ a, s = get_n_sound("results/data/12-09-2021_20-53-10_results.pkl")
 
 print(a)
 print(s)
+
+
+# # %%
+# # unused crap
+
+# for k in d:
+#     print(k)
+#     # print(d[k])
+#     d2 = d[k]
+#     for k2 in d2:
+#         print(k2)
+#         # print(d2[k2])
