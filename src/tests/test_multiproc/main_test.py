1cc10802ca546a32bd4416e7d39f2d3af5f957e6
Paul Straberger <p.straberger@gmail.com>
2021-12-25 18:11:51 +0100

    expanded testfunc a bit

--- src/tests/test_multiproc/main_test.py
+++ src/tests/test_multiproc/main_test.py
@@ -1,6 +1,13 @@
 from func import expensive_func
 from multiprocessing import Pool
 
-if __name__ == "main":
-    with Pool as p:
+def main():
+    with Pool() as p:
         res = p.map(expensive_func, [1,2,3,4,5,6,7,8])
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
+else:
+    raise Exception("woopsie, no run as main")
