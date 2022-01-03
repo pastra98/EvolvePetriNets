f040181395fd0fc02e002591628ffd67784662fa
Paul Straberger <p.straberger@gmail.com>
2022-01-03 11:28:15 +0100

    make it work for wangblows (yes there is a better way using filepath objs, but screw that)

--- results/analysis_scripts/analyze_top5_fit.py
+++ results/analysis_scripts/analyze_top5_fit.py
@@ -42,29 +42,31 @@ def rate_results(dir_to_analyze: str = None):
 
     try: last_visit = list(ratings.keys())[-1]; reached_lv = False
     except: last_visit = None; reached_lv = True
-    print(last_visit)
-    print(reached_lv)
-    
 
+    i = 0
+    print(last_visit)
     for r_nr in [1,2]:
         for root, dirs, files in os.walk(dir_to_analyze):
             # print(root)
             for file in files:
                 imgpath = os.path.join(root, file)
-                if not reached_lv:
+                if last_visit:
                     reached_lv = last_visit == imgpath
                 if (root.endswith("reports") and
-                    root.split("/")[-2].startswith(f"{r_nr}_") and
+                    root.split("\\")[-2].startswith(f"{r_nr}_") and
                     file.endswith("best_genome.png") and
                     reached_lv):
+                    i += 1
                     print(imgpath)
+                    last_visit = None
                     with PIL.Image.open(imgpath) as im:
-                        im.show()
+                        display(im)
                         while (usr_in := input("Quality of model: ")) not in ["1","2","3","4","exit"]:
                             print("rating must be 1, 2, 3 or 4")
                         if usr_in in ["1","2","3","4"]:
                             ratings[imgpath] = int(usr_in)
                             print(usr_in)
+                            print(f"evaluated: {i}")
                         elif usr_in == "exit":
                             save_and_quit(ratings)
                             return
@@ -73,3 +75,5 @@ def rate_results(dir_to_analyze: str = None):
 
 
 rate_results("results/data/test_top5_fitness_12-28-2021_21-12-03")
+
+# %%
