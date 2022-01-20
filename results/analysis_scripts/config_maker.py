5f7934d9a876e6c21c12ed03e063246c63e4f55f
Paul Straberger <p.straberger@gmail.com>
2022-01-20 20:39:24 +0100

    make config editing more efficient

--- results/analysis_scripts/config_maker.py
+++ results/analysis_scripts/config_maker.py
@@ -1,6 +1,7 @@
 # %%
 import sys # syspath fuckery to run from same location as main
 import os # set cwd to same as project cwd from vscode
+import shutil
 from pathlib import Path
 
 from pm4py.algo.conformance.alignments.petri_net.variants.state_equation_a_star import PARAM_SYNC_COST_FUNCTION
@@ -32,11 +33,35 @@ import json
 
 
 # %%
+# ---------- FUNC TO COMPARE PARAMS
+
+def compare_params(p1: dict, p2: dict):
+    changed = {}
+    for key in p1:
+        if p1[key] != p2[key]:
+            changed[key] = {"p1": p1[key], "p2": p2[key]}
+    pp.pprint(changed, indent=4, depth=3)
+
+# ---------- FUNC TO COMPARE PARAMS
+
 # make a new config folder
+
+# -------------------- BEGIN CONFIG CLASS
+
 class Config():
-    def __init__(self, name: str, save_to: str):
+    def __init__(self, name: str, save_to = "./configs"):
+        self.savepath = f"{save_to}/{name}"
+        try:
+            os.makedirs(f"{self.savepath}/params")
+        except Exception as e:
+            print(e)
+            if input("should I overwrite it? - ['yes' to overwrite]") == "yes":
+                print("okay, I'll delete the entire folder")
+                shutil.rmtree(self.savepath)
+                os.makedirs(f"{self.savepath}/params")
+            else:
+                print("Enter yes to overwrite, or choose a new name")
         self.name = name
-        self.save_to = save_to
         self.base_params = {}
         self.base_cfg = {
             "logpath": "pm_data/running_example.xes",
@@ -65,14 +90,36 @@ class Config():
         with open(fp) as f:
             name = fp.split("/")[-1].rstrip(".json")
             self.base_params[name] = {"base": json.load(f)}
+    
+    def load_baseparam_dict(self, name: str):
+        return self.base_params[name]["base"]
+
+    def new_baseparam(self, oldbasename: str, name: str, values: dict):
+        oldbase = self.load_baseparam_dict(oldbasename)
+        newbase = copy(oldbase)
+        for param, val in values.items():
+            newbase[param] = val
+        self.base_params[name] = {"base": newbase}
+        print(f"created new baseparams and added them: {name}")
+        compare_params(oldbase, newbase)
+
+    def compare_bases(self, base1: str, base2: str):
+        b1, b2 = self.load_baseparam_dict(base1), self.load_baseparam_dict(base2)
+        compare_params(b1, b2)
+
+    def print_base_params(self, name: str):
+        pp.pprint(self.load_baseparam_dict(name))
 
     def get_overview(self):
+        tot_num = 0
         for base_param_name in self.base_params:
             print(f"\n{base_param_name}\nchanging values:")
             for change_val in self.base_params[base_param_name]:
+                tot_num += 1
                 if change_val != "base":
                     print(f"{change_val} - new values:")
                     print(self.base_params[base_param_name][change_val]["values"])
+        print(f"you will create {tot_num} params")
     
     def new_param_list(self, base_params_name: str, param_to_change: str, new_values: list) -> list:
         if base_params_name in self.base_params:
@@ -105,13 +152,15 @@ class Config():
                 else:
                     cp_dict = bp_dict[p]
                     for i, cp in enumerate(cp_dict["params"]):
-                        val = cp_dict["values"][i]
-                        saves[f"{bp_name}_{p}_{val}"] = cp
+                        val = str(cp_dict["values"][i])
+                        for prob, fix in [(".","pt"), (",","_AND"), (" ","_")]:
+                            val = val.replace(prob, fix)
+                        saves[f"{bp_name}->{p}->{val}"] = cp
         final_cfg = {}
         final_cfg["name"] = self.name
         final_cfg["setups"] = []
         for savename, params in saves.items():
-            ppath = f"{self.save_to}/params/{savename}.json"
+            ppath = f"{self.savepath}/params/{savename}.json"
             with open(ppath, "w") as f:
                 json.dump(params, f, indent=4)
             new_setup = copy(self.base_cfg)
@@ -119,35 +168,36 @@ class Config():
             new_setup["parampath"] = ppath
             new_setup["n_runs"] = n_runs
             final_cfg["setups"].append(new_setup)
-        with open(f"{self.save_to}/config.json", "w") as f:
+        with open(f"{self.savepath}/config.json", "w") as f:
             json.dump(final_cfg, f, indent=4)
-
-# def compare_params(p1: dict, p2: dict):
-#     changed = {}
-#     for key in p1:
-#         if p1[key] != p2[key]:
-#             changed[key] = {"p1": p1[key], "p2": p2[key]}
-#     pp.pprint(changed, indent=4, depth=1)
+    
+# -------------------- END CONFIG CLASS
 
 # setup config for 2x, 3x, 4x
-x_cfg = Config("improve_fitness_2", "configs/improve_fitness_2")
-bp_paths = [
-    "configs/improve_fitness_2/base1x.json",
-    "configs/improve_fitness_2/base2x.json",
-    "configs/improve_fitness_2/base3x.json",
-    "configs/improve_fitness_2/base4x.json"
-]
-x_cfg.set_base_params(bp_paths)
+conf = Config("speciation_harder")
 
-# set param lists for all
-x_cfg.param_list_for_all_bases("perc_fit_traces_weight", [.5, 1, 1.5])
-x_cfg.param_list_for_all_bases("soundness_weight", [.5, 1, 1.5])
-x_cfg.param_list_for_all_bases("precision_weight", [.5, 1, 1.5])
-x_cfg.param_list_for_all_bases("generalization_weight", [.5, 1, 1.5])
-x_cfg.param_list_for_all_bases("simplicity_weight", [.5, 1, 1.5])
-x_cfg.param_list_for_all_bases("fraction_tasks_weight", [.5, 1, 1.5])
-# save with two runs (should be 152 total, for 76 configs)
-x_cfg.save_config(2)
+bp_paths = [
+    "configs/speciation_base_params/01_prob_tp.json",
+    "configs/speciation_base_params/03_prob_tp.json",
+    "configs/speciation_base_params/06_prob_tp.json",
+    "configs/speciation_base_params/harder.json"
+]
+conf.set_base_params(bp_paths)
+
+conf.param_list_for_all_bases("prob_t_t_conn", [
+    [0.1, 0],
+    [0.3, 0],
+    [0.6, 0]
+])
+
+
+conf.param_list_for_all_bases("average_trace_fitness_weight", [2, 4])
+conf.param_list_for_all_bases("soundness_weight", [2,4])
+conf.param_list_for_all_bases("simplicity_weight", [2,4])
+conf.param_list_for_all_bases("precision_weight", [2,4])
+conf.param_list_for_all_bases("t_exec_scoring_weight", [
+    [0, 0],
+])
 
 # %%
 # fitness func on 5 variations
@@ -245,3 +295,36 @@ for ppath in final_params:
 final_cfg = {"name": "test_top5_fitness", "setups": plist}
 with open("configs/improve_fitness/crazy_config.json", "w") as f:
     json.dump(final_cfg, f, indent=4)
+
+# %%
+# set param lists for all
+
+# x_cfg.param_list_for_all_bases("perc_fit_traces_weight", [.5, 1, 1.5])
+# x_cfg.param_list_for_all_bases("soundness_weight", [.5, 1, 1.5])
+# x_cfg.param_list_for_all_bases("precision_weight", [.5, 1, 1.5])
+# x_cfg.param_list_for_all_bases("generalization_weight", [.5, 1, 1.5])
+# x_cfg.param_list_for_all_bases("simplicity_weight", [.5, 1, 1.5])
+# x_cfg.param_list_for_all_bases("fraction_tasks_weight", [.5, 1, 1.5])
+# save with two runs (should be 152 total, for 76 configs)
+# x_cfg.save_config(2)
+
+# %%
+# # make a new set of baseparams
+# conf.set_base_params(bp_paths)
+# new_bp = [
+#     [{ 
+#     "prob_t_p_arc": [0.1, 0],
+#     "prob_p_t_arc": [0.1, 0]
+#     }, "01_prob_tp"],
+#     [{ 
+#     "prob_t_p_arc": [0.3, 0],
+#     "prob_p_t_arc": [0.3, 0]
+#     }, "03_prob_tp"],
+#     [{ 
+#     "prob_t_p_arc": [0.6, 0],
+#     "prob_p_t_arc": [0.6, 0]
+#     }, "06_prob_tp"]
+# ]
+
+# for bp in new_bp:
+#     conf.new_baseparam("harder", bp[1], bp[0])
