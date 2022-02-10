8db9ff5e6b65b9676940c0fbe42fa5c5eaaf36c1
Paul Straberger <p.straberger@gmail.com>
2022-02-10 16:21:05 +0100

    djlfksskdjf

--- src/tests/github_demo.py
+++ src/tests/github_demo.py
@@ -16,6 +16,7 @@ from pm4py.algo.conformance.tokenreplay import algorithm as token_replay
 from pm4py.objects.petri_net.obj import PetriNet, Marking
 from pm4py.objects.petri_net.utils.petri_utils import add_arc_from_to
 from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py import view_petri_net
 
 # load running example log
 lp = "pm_data/running_example.xes"
@@ -46,6 +47,7 @@ replayed_tr = token_replay.apply(
 )
 
 print(replayed_tr)
+view_petri_net(net, im, fm)
 
 # %%
 
