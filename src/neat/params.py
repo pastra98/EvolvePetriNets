72978782721363de490c5fc4369e56f5f215a690
Paul Straberger <p.straberger@gmail.com>
2024-05-30 17:53:47 +0200

    max arcs removed should be an int

--- src/neat/params.py
+++ src/neat/params.py
@@ -109,7 +109,7 @@ when atomic mutations are used.
 """
 # arc mutations
 prob_remove_arc: list[float, float]
-max_arcs_removed: float # in case of atomic mutation, this is set automatically to 1
+max_arcs_removed: int # in case of atomic mutation, this is set automatically to 1
 # Make a new arc
 prob_t_p_arc: list[float, float]
 prob_p_t_arc: list[float, float]
