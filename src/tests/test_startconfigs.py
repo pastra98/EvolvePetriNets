2e13fc6d86f099968067e0a118c15aba2d661f7f
Paul Straberger <p.straberger@gmail.com>
2024-06-14 22:25:34 +0200

    reorganize this POS a bit

--- src/tests/test_startconfigs.py
+++ src/tests/test_startconfigs.py
@@ -57,65 +57,6 @@ allnets = get_all_nets(log)
 
 # %%
 ################################################################################
-#################### NEW COMPATIBILITY FUNCTION ################################
-################################################################################
-
-
-# ---------- GET THE LONGEST VARIANT
-def find_longest_variant(log):
-    return max([len(t) for t in pm4py.stats.get_variants(log).keys()])
-
-lv = find_longest_variant(log)
-
-
-# ---------- DO AN EXTENSIVE PLAYOUT TEST
-def get_extensive_variants(pn, maxlen=None, debug=False):
-    from pm4py.algo.simulation.playout.petri_net.variants.extensive import apply as extensive_playout
-
-    net, im, fm = pn["net"], pn["im"], pn["fm"]
-    if maxlen:
-        res = extensive_playout(net, im, fm, parameters={"maxTraceLength": maxlen})
-    else:
-        res = extensive_playout(net, im, fm)
-    variants = list(pm4py.stats.get_variants(res).keys()) # return only the variants
-
-    # can delete the following block later
-    if debug:
-        pm4py.view_petri_net(net, debug=True)
-        print("variants in net:", len(variants))
-
-    return variants
-
-def number_overlapping_variants(variants0, variants1):
-    v1, v2 = set(variants0), set(variants1)
-    overlap = len(v1.intersection(v2))
-    fraction = overlap / len(v1.union(v2))
-    return fraction
-
-import pandas as pd
-
-
-def compare_all_nets(netlist):
-    netdict = dict()
-    for i, net in enumerate(netlist):
-        netdict[i] = get_extensive_variants(net)
-
-    # Prepare a DataFrame to store overlap scores
-    overlap_scores = pd.DataFrame(index=range(len(netlist)), columns=range(len(netlist)))
-    
-    # Calculate overlap scores for each combination of networks
-    for i in range(len(netlist)):
-        for j in range(i + 1, len(netlist)):  # Start from i+1 to avoid redundant and self comparisons
-            score = number_overlapping_variants(netdict[i], netdict[j])
-            overlap_scores.at[i, j] = score
-            overlap_scores.at[j, i] = score  # Mirror the score to avoid redundant function calls
-
-    # Fill NaN walues with 1 for the same net
-    overlap_scores = overlap_scores.fillna(1)
-    return overlap_scores
-
-# %%
-################################################################################
 #################### PLOT OVERLAP AS HEATMAP ###################################
 ################################################################################
 
@@ -204,101 +145,52 @@ def construct_genome_from_mined_net(net, im, fm, node_prefix: int):
     
     return g
 
-genetic_nets = build_mined_nets(allnets)
-
-#%%
-################################################################################
-#################### TESTING REACHABILITY GRAPH ################################
-################################################################################
-
-from pm4py.objects.petri_net.utils.reachability_graph import construct_reachability_graph
-
-# n = allnets[0]
-for n in allnets[:2] + [allnets[3]]:
-    tso = construct_reachability_graph(n["net"], n["im"], n["fm"], parameters={"max_elab_time": 1.0})
-    pm4py.vis.view_transition_system(tso)
-
-#%%
-################################################################################
-#################### COMPONENT OVERLAP SCORE ###################################
-################################################################################
-from pm4py.objects.petri_net.obj import PetriNet as pn
-from collections import Counter
-
-
-def format_tname(t):
-    # all hidden transitions are named "t"
-    return t.label if t.label in innovs.get_task_list() else "t"
-
-def add_md_to_net(net): # of course this is total bullshit because we should not do this to petri net objects
-    net["md"] = set()
-
-    for md in maximal_decomposition(net["net"], net["im"], net["fm"]):
-        a_multi_set = Counter() # multiset of arcs in the component
-
-        for a in md[0].arcs:
-            if type(a.source) == pn.Transition: # target must be a place
-                # pack into iterable (list) to avoid unpacking
-                a_multi_set.update([(format_tname(a.source), "p")]) # only one place per component
-            else: # source must be a place, target must be a transition
-                a_multi_set.update([("p", format_tname(a.target))])
-
-        # convert multiset to tuple to make it hashable, order of tuples must be the same
-        if res := tuple(sorted(a_multi_set.items())): # only add non-empty components
-            net['md'].add(res)
-
-for net in allnets:
-    add_md_to_net(net)
-
-# pm4py.view_petri_net(allnets[0]['net'])
-allnets[0]['md']
-
-#%%
-
-def md_similarity(net1, net2):
-    # return len(net1["md"].intersection(net2["md"]))/len(net1["md"].union(net2["md"]))
-    return len(net1["md"] & net2["md"]) / len(net1["md"] | net2["md"])
-
-# compute the similarity between the first 3 nets
-for i in range(3):
-    g1 = construct_genome_from_mined_net(allnets[i]["net"], i+1)
-    for j in range(i+1, 3):
-        print(md_similarity(allnets[i], allnets[j]))
-        g2 = construct_genome_from_mined_net(allnets[j]["net"], j+1)
-        print(g1.component_compatibility(g2))
-        print()
-
-n1, n2, n3 = allnets[:3]
-
-# %%
-################################################################################
-#################### TESTING ATOMIC MUTATIONS ##################################
-################################################################################
-
 def reload_module_and_get_fresh_genome():
     reload(genome)
     nets = mine_bootstrapped_nets(log)
     return build_mined_nets(nets)
 
+genetic_nets = build_mined_nets(allnets)
 
-fresh_genomes = reload_module_and_get_fresh_genome()
-tg = fresh_genomes[0].clone()
+# %%
+# %%
+################################################################################
+#################### TEST CROSSOVER ###########################################
+################################################################################
+from pprint import pprint
 
-# net, im, fm = tg.build_petri()
-# pm4py.view_petri_net(net)
-tg.mutate(0)
+g: genome.GeneticNet = reload_module_and_get_fresh_genome()[1] # alpha algo
+
+g.build_petri.cache_clear()
+net, im, fm = g.build_petri()
+pm4py.view_petri_net(net, im, fm)
+pprint(g.get_unique_component_set())
+# print('fitness before deletions')
+# g.evaluate_fitness(log)
+# print(g.fitness)
+
+
 
 # %%
 ################################################################################
-#################### REACHABILITY GRAPH SUCKS ##################################
+#################### TEST NODE REACHABILITY FITNESS ############################
 ################################################################################
-from pm4py.convert import convert_to_reachability_graph
-rgs = []
+# need to define my own func bc. pm4py does not add labels
 
-for n in allnets[:4]:
-    pm4py.view_petri_net(n["net"], debug=True)
-    rg = convert_to_reachability_graph(n["net"], n["im"], n["fm"])
-    rgs.append(rg)
+def build_digraph(net: genome.GeneticNet):
+    graph = nx.DiGraph()
+    for p in net.places:
+        graph.add_node(p)
+    for t in net.transitions:
+        graph.add_node(t)
+    for a in net.arcs.values():
+        graph.add_edge(a.source_id, a.target_id)
+    return graph
+
+nx_graph = build_digraph(g)
+nx.draw(nx_graph, with_labels=True)
+
+nx.descendants(nx_graph, "start")
 
 # %%
 ################################################################################
@@ -399,96 +291,3 @@ def plot_averages(df):
     plt.show()
 
 plot_averages(df)
-
-# %%
-################################################################################
-#################### TEST CROSSOVER ###########################################
-################################################################################
-from pprint import pprint
-
-g: genome.GeneticNet = reload_module_and_get_fresh_genome()[1] # alpha algo
-
-g.build_petri.cache_clear()
-net, im, fm = g.build_petri()
-pm4py.view_petri_net(net, im, fm)
-pprint(g.get_unique_component_set())
-# print('fitness before deletions')
-# g.evaluate_fitness(log)
-# print(g.fitness)
-
-
-
-# %%
-from pm4py.objects.petri_net.utils.networkx_graph import create_networkx_directed_graph
-from pm4py.objects.petri_net.utils.networkx_graph import create_networkx_undirected_graph
-from networkx import number_connected_components
-import matplotlib.pyplot as plt
-import networkx as nx
-
-reload(pm4py.objects.petri_net.utils.networkx_graph)
-
-# nx_graph, id = create_networkx_directed_graph(net)
-# nx_graph, *_ = create_networkx_undirected_graph(net, None, None)
-nx_graph, *_ = create_networkx_directed_graph(net)
-
-pos = nx.spring_layout(nx_graph)
-nx.draw(nx_graph, pos, with_labels=True, node_size=500, node_color="lightblue", font_size=10, font_color="black")
-plt.show()
-
-# num_components = number_connected_components(nx_graph)
-# print(num_components)
-
-# %%
-# need to define my own func bc. pm4py does not add labels
-
-def build_digraph(net: genome.GeneticNet):
-    graph = nx.DiGraph()
-    for p in net.places:
-        graph.add_node(p)
-    for t in net.transitions:
-        graph.add_node(t)
-    for a in net.arcs.values():
-        graph.add_edge(a.source_id, a.target_id)
-    return graph
-
-nx_graph = build_digraph(g)
-nx.draw(nx_graph, with_labels=True)
-
-nx.descendants(nx_graph, "start")
-
-# %%
-reload(netobj)
-
-et = genome.GTrans()
-
-sp = genome.GPlace(id="start")
-
-a = genome.GArc(source_id=sp.id, target_id=et.id)
-print(et)
-print(sp)
-print(a)
-
-# %%
-a = {1,2,3}
-b = {0,2}
-
-a = a.union(b)
-print(a)
-# %%
-def print_next_three_elements(lst):
-    length = len(lst)
-    for i in range(length):
-        next_elements = [(i + j) % length for j in range(1, 4)]
-        print([lst[index] for index in next_elements])
-
-# Example usage
-lst = [1, 2, 3, 4, 5]
-print_next_three_elements(lst)
-
-# %%
-import random as rd
-
-d = {"a":1, "b":2}
-
-d.pop("a", None)
-print(d)
