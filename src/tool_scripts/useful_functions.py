e5dfa2463a1ef593d1654c325b9247c3bffd4acb
Paul Straberger <p.straberger@gmail.com>
2024-07-05 12:03:57 +0200

    move some commonly used stuff into useful_functions

---
+++ src/tool_scripts/useful_functions.py
@@ -0,0 +1,67 @@
+from neat import params, genome, initial_population
+
+import pm4py
+from pm4py.stats import get_variants
+from pm4py.algo.conformance.tokenreplay.variants.token_replay import apply as get_replayed_traces
+from pm4py.objects.petri_net.obj import PetriNet as pn
+
+from pprint import pprint
+from IPython.display import display
+import pickle
+from importlib import reload
+
+params.load('../params/testing/test_params.json')
+log = pm4py.read_xes("../pm_data/running_example.xes")
+log
+
+# path = "E:/migrate_o/github_repos/EvolvePetriNets/results/data/bs_oops_now_actual_perc_fit_tr_06-25-2024_15-30-29/whatever/1_06-25-2024_15-30-36/reports/best_genome.pkl"
+# path = "E:/migrate_o/github_repos/EvolvePetriNets/results/data/bs__perc_fit_tr_gen_prec_06-25-2024_15-59-21/whatever/1_06-25-2024_15-59-31/reports/best_genome.pkl"
+
+def load_genome(path):
+    with open(path, 'rb') as file:
+        g: genome.GeneticNet = pickle.load(file)
+    g.clear_cache()
+    g.evaluate_fitness(log)
+    return g
+
+def show_genome(g: genome.GeneticNet):
+    display(g.get_gviz())
+
+def get_aligned_traces(g: genome.GeneticNet, log, print=False):
+    default_params = {"show_progress_bar": False}
+    g.clear_cache()
+    net, im, fm = g.build_petri()
+    aligned_traces = get_replayed_traces(log, net, im, fm, default_params)
+    if print:
+        pprint(aligned_traces)
+    return aligned_traces
+
+def get_log_variants(log, print=False):
+    variants = get_variants(log)
+    if print:
+        for variant in variants:
+            print(' -> '.join(variant))
+    return [list(v) for v in variants.keys()]
+
+
+def eval_and_print_fitness(g: genome.GeneticNet, log):
+    g.clear_cache()
+    g.evaluate_fitness(log)
+    print("perc_fit_traces", g.perc_fit_traces)
+    print("average_trace_fitness", g.average_trace_fitness)
+    print("log_fitness", g.log_fitness)
+    print("is_sound", g.is_sound)
+    print("precision", g.precision)
+    print("generalization", g.generalization)
+    print("simplicity", g.simplicity)
+    print("fraction_used_trans", g.fraction_used_trans)
+    print("fraction_tasks", g.fraction_tasks)
+    print("execution_score", g.execution_score)
+    print("---> overall fitness", g.fitness, "\n")
+
+
+def reset_ga():
+    neat_modules = [params, genome, initial_population]
+    for module in neat_modules:
+        reload(module)
+    params.load('../params/testing/test_params.json')
